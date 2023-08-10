40fdd42bedd03d6b3988e32600a09d44b608f49d
ryan7ait <aitmechedalryan7@gmail.com>
2023-08-10 17:33:53 +0100

    add prompt

--- README.md
+++ README.md
@@ -14,13 +14,7 @@ Adhkari is an extenstion made for fun, but it cand reminde you to Mention  God w
 - Send You dhiker every 1 minute
 - Don't start until you activated 
 
-> The overriding design goal for Markdown's
-> formatting syntax is to make it as readable
-> as possible. The idea is that a
-> Markdown-formatted document should be
-> publishable as-is, as plain text, without
-> looking like it's been marked up with tags
-> or formatting instructions.
+
 
 
 
@@ -42,9 +36,7 @@ And Voilaa
 
 
 
-## License
 
-MIT
 
 **It's all Free  !**
 
--- src/extension.ts
+++ src/extension.ts
@@ -12,29 +12,72 @@ export function activate(context: vscode.ExtensionContext) {
 		'استغفر الله',
 		'سُبْحَانَ اللهِ العَظِيمِ وَبِحَمْدِهِ',
 		'لَا إلَه إلّا اللهُ وَحْدَهُ لَا شَرِيكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلُّ شَيْءِ قَدِيرِ. ',
-		'الْحَمْدُ لِلَّهِ حَمْدًا كَثِيرًا طَيِّبًا مُبَارَكًا فِيهِ.'
+		'الْحَمْدُ لِلَّهِ حَمْدًا كَثِيرًا طَيِّبًا مُبَارَكًا فِيهِ.',
+		'سُبْحَانَ اللَّهِ وَبِحَمْدِهِ ',
+		'سُبْحَانَ اللَّهِ وَبِحَمْدِهِ ، سُبْحَانَ اللَّهِ الْعَظِيمِ',
+		'الْحَمْدُ للّهِ رَبِّ الْعَالَمِينَ',
+		''
 	];
 
+
+
+	let disposable = vscode.commands.registerCommand('adhkari.adhkari', async () => {
+
+        const intervalResponse = await vscode.window.showInputBox({
+            prompt: "Please choose an intervel to recive a dhiker in seconds (ex: every 60seconds)",
+            value: "60", // Default value
+            validateInput: (value: string) => {
+                if (!/^\d+$/.test(value) || parseInt(value) <= 0) {
+                    return "Please enter a valid positive integer.";
+                }
+                return null;
+            }
+        });
+
+        if (!intervalResponse) {
+            return; // User canceled the input
+        }
+
+        const durationResponse = await vscode.window.showInputBox({
+            prompt: "Please choose an time to stop reciving dhiker in minutes (ex: stop after 10 minute)",
+            value: "10", // Default value
+            validateInput: (value: string) => {
+                if (!/^\d+$/.test(value) || parseInt(value) <= 0) {
+                    return "Please enter a valid positive integer.";
+                }
+                return null;
+            }
+        });
+
+        if (!durationResponse) {
+            return; // User canceled the input
+        }
+
+		const intervalInSeconds: number = parseInt(intervalResponse);
+        const durationInMinutes: number = parseInt(durationResponse);
+
+        let dhiker = adhkar[Math.floor(Math.random() * adhkar.length)];
+
+		      
+
+        vscode.window.showInformationMessage(dhiker);
+
+        let myInterval = setInterval(() => {
+            dhiker = adhkar[Math.floor(Math.random() * adhkar.length)];
+            vscode.window.showInformationMessage(dhiker);
+        }, intervalInSeconds * 1000);
+
+        setTimeout(() => {
+            clearInterval(myInterval);
+        }, durationInMinutes * 60 * 1000);
+
+    });
+
+	
 	let dhiker = adhkar[Math.floor(Math.random() * 10)];
 
 	
-	let disposable = vscode.commands.registerCommand('adhkari.adhkari', () => {
-		
-		vscode.window.showInformationMessage(dhiker);
-
-
-		let  myInteval = setInterval(() => {
-			let dhiker = adhkar[Math.floor(Math.random() * 10)];
-
-			vscode.window.showInformationMessage(dhiker);
-		}, 60000);
-
-
-		setTimeout(() => {
-			clearInterval(myInteval);
-		},60000 * 10);
-
-	});
+	
 
 
 
